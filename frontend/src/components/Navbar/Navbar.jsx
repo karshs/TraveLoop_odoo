a8ae33899abbb3350d5d7103182592e6ab7f90f3
dev <devdtailor555@gmail.com>
2026-05-10 17:28:36 +0530

    fix(navbar): navbar changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.scss';
 
 const NAV_LINKS = [
